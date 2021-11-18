c1a78818875c683b3c839f32785f63253ee00764
LauraVeM1 <lvegam1@toluca.tecnm.mx>
2021-11-18 11:29:25 -0600

    pagina principal , header  footer

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'templates/header.php';
+?>
+<div class="container-fluid">
+    <h1 style="text-align: center;">Bienvenido</h1>
+</div>
+
+<?php
+include 'templates/footer.php';
+?>
---
+++ templates/footer.php
@@ -0,0 +1,47 @@
+<footer>
+        <div class="row">
+            <div class="col-md-4">
+                <h3>Redes Sociales</h3>
+                <ul>
+                    <li>
+                        <img src="img/instagram.png" alt="instagram" height="50px" width="50px"> guerrablog
+                    </li>
+                    <li>
+                        <img src="img/email.png" alt="email" height="50px" width="50px"> guerrablog@contactame.com
+                    </li>
+                    <li>
+                        <img src="img/twitter.png" alt="twitter" height="50px" width="50px"> guerrablog
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+                <h3>Colaboradores</h3>
+                <ul>
+                    <li>
+                        <h6>Becerril Padilla Guadalupe</h6>
+                    </li>
+                    <li>
+                        <h6>Flores Ávila Carlos Alberto</h6>
+                    </li>
+                    <li>
+                        <h6>González Santana Juan Pablo</h6>
+                    </li>
+                    <li>
+                        <h6>Nuño Rodríguez Nestor Daniel</h6>
+                    </li>
+                    <li>
+                        <h6>Vega Mondragón Laura</h6>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        <h6>2021 GuerraBlog <i class="fa fa-registered"></i></h6>
+</footer>
+
+
+</body>
+</html>

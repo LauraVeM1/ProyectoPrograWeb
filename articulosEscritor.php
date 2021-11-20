7e886ea960783c50b4503c878840c35ea332f962
nestor-nurod <71893392+nestor-nurod@users.noreply.github.com>
2021-11-20 17:39:27 -0600

    Add files via upload
    Se añaden las pantallas para el escritor

---
+++ articulosEscritor.php
@@ -0,0 +1,22 @@
+<?php require_once 'templates/header.php' ?>
+<div class="container-fluid py-5">
+    <h1 class="titulos">Mis artículos</h1>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="" alt="IMG-CARD">
+                    <div class="card-body">
+                        <p class="card-text text-center">Aqui va una breve descripcion del articulo</p>
+                        <button class="btn-brown">Editar</button>
+                        <button class="btn-brown">Publicar</button>
+                        <button class="btn-brown">Eliminar</button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<?php require_once 'templates/footer.php' ?>

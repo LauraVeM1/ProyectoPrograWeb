89748bc48cbc9fb7c3fe657b937ea6bac6c4116f
nestor-nurod <71893392+nestor-nurod@users.noreply.github.com>
2021-11-20 16:10:22 -0600

    Add files via upload

---
+++ articulosEscritor.php
@@ -0,0 +1,22 @@
+<?php require_once 'templates/header.php' ?>
+<div class="container-fluid py-5">
+    <h1 class="titulos">Mis artículos</h1>
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="img/Messi.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text text-center">Andres Lionel es el mejor futbolista de la historia pese a quien le pese y que se vaya a la verga quien no lo crea.</p>
+                        <button class="btn-brown">Editar</button>
+                        <button class="btn-brown">Publicar</button>
+                        <button class="btn-brown">Eliminar</button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
+<?php require_once 'templates/footer.php' ?>
---
+++ escritorHome.php
@@ -0,0 +1,22 @@
+<?php require_once 'templates/header.php' ?>
+<div class="container-fluid p-5">
+    <div class="container text-center">
+        <div class="pt-5 pb-5 esc-home">
+            <figure><img src="img/user.png" alt="User"></figure>
+            <h2 class="m-0">Bienvenido</h2>
+            <h3>Usuario</h3>
+            <div class="container">
+            <div class="row">
+                <div class="col-md-6 mt-5 mb-5">
+                    <a href="#" class="btn-esc">Mis artículos</a>
+                </div>
+                <div class="col-md-6 mt-5 mb-5">
+                <a href="#" class="btn-esc"><i class="fa fa-plus-circle"></i>Crear artículo</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once 'templates/footer.php' ?>

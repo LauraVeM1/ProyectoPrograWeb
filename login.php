30c76ff7be15f5bfd3eef396ee5972bcecd31192
Nestornuno <nestor.nurod@outlook.com>
2021-11-20 19:53:43 -0600

    Se agrega la pantalla de login

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php require_once 'templates/header.php'?>
+
+<div class="container-fluid">
+    <div class="row">
+    <figure class="text-center" style="margin:0;" ><img src="img/user_1.png" alt="Usuario"><p class="text-dark mb-0">Iniciar sesion a GuerraBlog</p></figure>
+    </div>
+</div>
+
+<?php require_once 'templates/footer.php'?>

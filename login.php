d68230aa772e81f0414022b44b6f5b86e789c743
Nestornuno <nestor.nurod@outlook.com>
2021-11-20 20:36:51 -0600

    Se agrega el formulario para el login

--- login.php
+++ login.php
@@ -1,9 +1,26 @@
-<?php require_once 'templates/header.php'?>
+<?php require_once 'templates/header.php' ?>
 
-<div class="container-fluid">
-    <div class="row">
-    <figure class="text-center" style="margin:0;" ><img src="img/user_1.png" alt="Usuario"><p class="text-dark mb-0">Iniciar sesion a GuerraBlog</p></figure>
-    </div>
+<div class="container-fluid p-5">
+    <figure class="text-center" style="margin:0;"><img class="img-login" src="img/user_1.png" alt="Usuario">
+        <p class="text-dark lead font-weight-bold mb-0">Iniciar sesion a GuerraBlog</p>
+    </figure>
+
+    <form class="text-center login p-4" method="post" action="">
+        <div class="form-group text-justify">
+          <label>Correo Electrónico</label><br>
+          <input type="text" name="txtUsuario" class="form-control" id="usuario" placeholder="@Correo" required>
+        </div>
+        <div class="form-group text-justify">
+          <label>Contraseña</label><br>
+          <input type="password" class="form-control" name="txtpass" id="pass" placeholder="Contraseña" required>
+        </div>
+        <div class="form-group">
+          <input type="submit" class="btn-login btn-brown" name="btnLogin" id="btnLogin" value="Ingresar">
+        </div>
+        <div class="form-group text-right">
+            <button style="padding: 0" class="btn font-weight-bold">Registrarse</button>
+        </div>
+    </form>
 </div>
 
-<?php require_once 'templates/footer.php'?>
+<?php require_once 'templates/footer.php' ?>

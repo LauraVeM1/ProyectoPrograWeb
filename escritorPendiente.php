cfdd760096391ceb943fa4306af14e57a90dabe0
Nestornuno <nestor.nurod@outlook.com>
2021-11-24 11:00:19 -0600

    Se añade pantalla para escritor pendiente

---
+++ escritorPendiente.php
@@ -0,0 +1,19 @@
+<?php
+include 'templates/header.php';
+?>
+<div class="container-fluid">
+    <div class="container text-center p-4">
+        <div class="card aviso" style="width: 24rem;">
+            <img class="card-img-top" src="img/reloj.gif" alt="Reloj">
+            <div class="card-body">
+                <p class="card-text">Su cuenta esta pendiente, esta siendo revisada por alguno de nuestros administradores.</p>
+                <p class="card-text">En un maximo de 24 horas su cuenta sera validada para pertenecer a nuestro grupo de escritores</p>
+                <a href="login.php" style="text-decoration: none;" class="btn-brown">Regresar</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'templates/footer.php';
+?>

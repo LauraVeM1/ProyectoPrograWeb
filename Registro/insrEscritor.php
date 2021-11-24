2a431891a3efbc6aa3cc3ce414b5b6f66ff87412
GuadalupeBP <85042073+GuadalupeBP@users.noreply.github.com>
2021-11-24 14:49:32 -0600

    Add files via upload
    Seccion de registro terminada

---
+++ Registro/insrEscritor.php
@@ -0,0 +1,33 @@
+<?php
+    include '../templates/header.php';
+    include '../templates/dbcon.php';
+    $message='';
+        
+        $nombre=$_POST['nombre'];
+        $pass = $_GET['pass'];
+        $email = $_GET['email'];
+        $apellido=$_POST['apellido'];
+        $edad=$_POST['edad'];
+        $rfc=$_POST['rfc'];
+        $direccion=$_POST['direccion'];
+        $telefono=$_POST['telefono'];
+        $referencias=$_POST['referencias'];
+
+
+        if(!empty($email)&&!empty($pass) &&
+            !empty($_POST['nombre'])&& !empty($_POST['apellido']) &&
+            !empty($_POST['edad'])&&!empty($_POST['rfc']) &&
+            !empty($_POST['direccion'])&&!empty($_POST['telefono'])
+            &&!empty($_POST['referencias'])){
+                $sql="INSERT INTO usuario (nombre, apellido, correo, password, rfc, edad, direccion, telefono, referencias, estatus, id_rol) VALUES ('$nombre', '$apellido', '$email', '$pass', '$rfc', '$edad', '$direccion', '$telefono', '$referencias', 'pendiente', '1')";
+                $stmt = $con->prepare($sql);
+                if($stmt->execute()){
+                    $message = 'Successfully created new user';
+                }else{
+                    $message='Sorry';
+                }
+                echo '<script>window.location="..//Sesion/login.php"</script>';
+        }
+        
+    
+?>
---
+++ Registro/terminosLector.php
@@ -0,0 +1,26 @@
+<?php
+    include '../templates/header.php';
+    $email=$_GET['email'];
+    $pass=$_GET['pass'];
+    $message='';
+?>
+<div class="container-fluid">
+        <h1 style="text-align: center; font-weight: bold; color:black;">Términos y condiciones de uso</h1>
+        <div class="jumbotron w-50 mx-auto bg-dark" >
+            <div class="row justify-content-center">
+                <h4 class="col-sm-10 text-white">Está prohibido realizar comentarios inadecuados en un artículo publicado y en caso 
+                de realizar está acción será sancionado o suspendido y al no aceptar estás condiciones 
+                deberá abstenerse de usar la plataforma.</h4>
+                <div class="col-sm-10" style="padding-top:20px">
+                    <button id="btnRegresar" class="btn btn-danger" type="button" name="regresar" onclick="location.href='<?php echo "../Registro/RegistroLector.php?email=".$email."&pass=".$pass ?>'">
+                        <span class="fa fa-arrow-left"></span> Regresar
+                    </button>
+                </div>
+            </div>
+            
+            
+        </div>
+</div>
+<?php
+    include '../templates/footer.php';
+?>
